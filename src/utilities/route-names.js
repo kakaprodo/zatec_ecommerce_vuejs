c8f00602ef11747bcc6148e48322ed7538dfabaa
kakaprodo <promskakaprodo45@gmail.com>
2022-02-18 16:18:21 +0200

    chore: remove all vue default files

---
+++ src/utilities/route-names.js
@@ -0,0 +1,13 @@
+const RouteName = {
+  INDEX: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  SINGLE_PRODUCT: "/single-product",
+  USER_PROFILE: "/user-profile",
+
+  //profile pages
+  USER_TRANSACTIONS: "/",
+  USER_PURCHASES: "/user-purchases",
+};
+
+export default RouteName;
