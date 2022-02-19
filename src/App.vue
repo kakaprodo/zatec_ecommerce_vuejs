6b2b95a3fb345e3e0cf047f8566f9122bc0f9aff
kakaprodo <promskakaprodo45@gmail.com>
2022-02-20 00:42:16 +0200

    Error handling: rename the error container component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
-import ErrorContainer from "./components/shared/ErrorContainer.vue";
+import HttpStatusContainer from "./components/shared/HttpStatusContainer.vue";
 </script>
 
 <template>
   <div class="font-sans antialiased bg-gray-100 min-h-screen">
-    <ErrorContainer />
+    <HttpStatusContainer />
     <RouterView />
   </div>
 </template>
